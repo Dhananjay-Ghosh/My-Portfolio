18bd16c191d141110bd3243846b29bf55d1e784d
DHANANJAY GHOSH <97255546+Dhananjay-Ghosh@users.noreply.github.com>
2023-06-08 12:46:59 +0530

    Update index.js
    changes to HashRouter from BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
@@ -10,9 +10,9 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
